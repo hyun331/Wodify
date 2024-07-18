b5ac47732ae6b3f5ee75a5b9fb46e99b43d8ba2f
qkdrmawll <eunji8106@naver.com>
2024-07-18 14:39:47 +0900

    Reservation 생성

---
+++ src/main/java/com/soocompany/wodify/reservation/domain/Reservation.java
@@ -0,0 +1,12 @@
+package com.soocompany.wodify.reservation.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Reservation {
+    @Id
+    @GeneratedValue
+    private Long id;
+}
