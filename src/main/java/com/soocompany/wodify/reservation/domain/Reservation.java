94ab6f61ecfc85e3c71bf2a433c877be595267a6
qkdrmawll <eunji8106@naver.com>
2024-07-18 23:16:05 +0900

    Reservation Entity설계

--- src/main/java/com/soocompany/wodify/reservation/domain/Reservation.java
+++ src/main/java/com/soocompany/wodify/reservation/domain/Reservation.java
@@ -1,12 +1,36 @@
 package com.soocompany.wodify.reservation.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Reservation {
     @Id
     @GeneratedValue
     private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+    @Column(nullable = false)
+    private LocalTime time;
+//    @JoinColumn("wod_id")
+//    @ManyToOne
+//    private Wod wod;
+
+//    @JoinColumn("coach_id")
+//    @ManyToOne
+//    private Member coach;
+    @Column(nullable = false)
+    private int maximumPeople;
+    @Column(nullable = false)
+    private int availablePeople;
 }
