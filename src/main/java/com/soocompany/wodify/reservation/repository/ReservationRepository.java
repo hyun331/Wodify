4681ecc758d810149eb9a88ab54235df97b9b520
qkdrmawll <eunji8106@naver.com>
2024-07-24 10:53:26 +0900

    Feat : Reservation 박스별, 날짜별 조회 구현 및 paging처리+ ResponseEntity 사용

--- src/main/java/com/soocompany/wodify/reservation/repository/ReservationRepository.java
+++ src/main/java/com/soocompany/wodify/reservation/repository/ReservationRepository.java
@@ -2,16 +2,18 @@ package com.soocompany.wodify.reservation.repository;
 
 import com.soocompany.wodify.box.domain.Box;
 import com.soocompany.wodify.reservation.domain.Reservation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
-    List<Reservation> findByBoxAndAndDelYn(Box box, String delYn);
+    Page<Reservation> findByBoxAndAndDelYn(Box box, String delYn, Pageable pageable);
 
-
+    Page<Reservation> findByBoxAndAndDateAndAndDelYn(Box box, LocalDate date, String delYn, Pageable pageable);
     Optional<Reservation> findByIdAndDelYn(Long id,String delYn);
 }
