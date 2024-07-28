1ed52a2c85fc4edf4573bbffc76ae2b1eb493ab2
qkdrmawll <eunji8106@naver.com>
2024-07-28 17:33:43 +0900

    feat : 정지정보 등록 및 조회 기능 추가

---
+++ src/main/java/com/soocompany/wodify/holding_info/dto/HoldingInfoResDto.java
@@ -0,0 +1,22 @@
+package com.soocompany.wodify.holding_info.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HoldingInfoResDto {
+    private Long id;
+    private Long memberId;
+    private String memberName;
+    private String memberEmail;
+    private LocalDate holdingStart;
+    private LocalDate holdingEnd;
+}
