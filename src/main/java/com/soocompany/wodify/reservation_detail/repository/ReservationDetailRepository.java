1fac7df75331c0f028910142e793b1a8414adf01
qkdrmawll <eunji8106@naver.com>
2024-07-28 15:11:42 +0900

    feat : reservation삭제 reservaiton detail 삭제 추가

--- src/main/java/com/soocompany/wodify/reservation_detail/repository/ReservationDetailRepository.java
+++ src/main/java/com/soocompany/wodify/reservation_detail/repository/ReservationDetailRepository.java
@@ -1,6 +1,9 @@
 package com.soocompany.wodify.reservation_detail.repository;
 
+import com.soocompany.wodify.member.domain.Member;
 import com.soocompany.wodify.reservation_detail.domain.ReservationDetail;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,5 @@ import java.util.Optional;
 @Repository
 public interface ReservationDetailRepository extends JpaRepository<ReservationDetail,Long> {
     Optional<ReservationDetail> findByIdAndDelYn(Long id, String delYn);
+    Page<ReservationDetail> findByMemberAndDelYn(Member member, String delYn, Pageable pageable);
 }
