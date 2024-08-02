9e6f4deb4c328638ad632d766d914c54a5bed159
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-08-02 20:27:34 +0900

    Add files via upload

---
+++ src/main/java/com/soocompany/wodify/wodDetail/domain/WodDetail.java
@@ -0,0 +1,32 @@
+package com.soocompany.wodify.wodDetail.domain;
+
+import com.soocompany.wodify.common.BaseEntity;
+import com.soocompany.wodify.wod.domain.Wod;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WodDetail extends BaseEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "wod_id")
+    private Wod wod;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 3000)
+    private String contents;
+}
