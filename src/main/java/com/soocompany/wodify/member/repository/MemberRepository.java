e0235141022f10016786f3466423f129074457e4
hyun331 <sus03319@gmail.com>
2024-08-05 00:29:09 +0900

    FEAT: 대표 - 박스 폐업

--- src/main/java/com/soocompany/wodify/member/repository/MemberRepository.java
+++ src/main/java/com/soocompany/wodify/member/repository/MemberRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -24,4 +25,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Page<Member> findByBoxAndRoleAndDelYn(Pageable pageable, Box box, Role role, String delYn);
 
     Optional<Member> findByEmailAndBoxAndDelYn(String email, Box box, String delYn);
+
+    List<Member> findByBoxAndDelYn(Box box, String n);
 }
