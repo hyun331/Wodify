a4a9b21633cdd52259b088c4eb0aa0cf76dc4013
sueroku <eraltndusn@gmail.com>
2024-08-07 21:49:03 +0900

    홈화면, 헤더

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import { memberRouter } from './member/MemberRouter';
-const routes = [
-    ...memberRouter
-    
+import HomeView from '@/views/common/HomeView.vue';
 
+const routes = [
+    {
+        path: '/',
+        name: 'HomeView',
+        component: HomeView
+    },
+    ...memberRouter
 ]
 
 
