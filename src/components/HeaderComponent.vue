6a567a98dbec341e8007a2f6959c3bd95ec194f0
hyun331 <sus03319@gmail.com>
2024-08-08 10:39:40 +0900

    FEAT: 회원가입 선택화면

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,4 +1,16 @@
+<template>
+    <v-app-bar>
+        <v-container>
+            <v-row align="center">
+                <v-col class="d-flex justify-start">
+                    <h1>HEADER</h1>
+                    <v-btn @click="kakaoLogin">LOGIN</v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
 
+    </v-app-bar>
+    
 </template>
 <script>
 // import axios from 'axios';
--- src/views/member/ChooseRegister.vue
+++ src/views/member/ChooseRegister.vue
@@ -1,27 +1,34 @@
 <template>
     <div class="background">
         <v-container>
-            <v-row class="d-flex justify-content-between mt-5">
+            
+            <v-row>
                 <v-col cols="6">
-                    <v-img :src="require('@/assets/wodify.png')" alt="WODIFY"  contain></v-img>
+                    <v-img :src="require('@/assets/wodify.png')" alt="WODIFY" sizes="70%" contain></v-img>
                 </v-col>
-                <v-col cols="6">
+                <v-col cols="6" hidden>
 
                 </v-col>
             </v-row>
+
+            <!-- <div class="rubikMonoOne">
+                SIGN UP
+            </div> -->
             <v-row justify="center" class="d-flex justify-content-between mt-5" style="width: 80%; margin: 0 auto;">
                 <v-col cols="6">
-                    <v-card >
-                        <v-img :src="require('@/assets/wodify.png')" alt="일반회원가입 이미지" height="400" contain></v-img>
-                        <br>
-                        <v-btn style="width: 90%;">일반회원가입</v-btn>
+                    <v-card  @mouseover="changeImage('user')" @mouseleave="resetImage('user')">
+                        <v-img :src=userImage alt="일반회원가입 이미지"  contain></v-img>
+                        <v-card-title justify="center" class="text-cneter text-h5">
+                            일반회원가입
+                        </v-card-title>
                     </v-card>
                 </v-col>
                 <v-col cols="6">
-                    <v-card>
-                        <v-img :src="require('@/assets/wodify.png')" alt="코치회원가입 이미지" height="400" contain></v-img>
-                        <br>
-                        <v-btn style="width: 90%;">코치회원가입</v-btn>
+                    <v-card  @mouseover="changeImage('coach')"  @mouseleave="resetImage('coach')"> 
+                        <v-img :src=coachImage alt="코치회원가입 이미지"  contain></v-img>
+                        <v-card-title justify="center" class="text-cneter text-h5">
+                            코치회원가입
+                        </v-card-title>
                     </v-card>
                 </v-col>
             </v-row>
@@ -37,6 +44,31 @@
 <script>
 
 export default {
+    data(){
+        return{
+            defaultImage: require('@/assets/home.png'),
+            userImage : require('@/assets/home.png'),
+            coachImage : require('@/assets/home.png'),
+            hoverImage: require('@/assets/home_color2.png'),
+
+        }
+    },
+    methods:{
+        changeImage(role){
+            if(role === 'user'){
+                this.userImage = this.hoverImage;
+            }else{
+                this.coachImage = this.hoverImage;
+            }
+        },
+        resetImage(role){
+            if(role === 'user'){
+                this.userImage = this.defaultImage;
+            }else{
+                this.coachImage = this.defaultImage;
+            }
+        }
+    }
 
 }
 </script>
