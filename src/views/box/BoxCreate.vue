fb5a895dbbd842ee0f1a50ca8e2d39d651a7baf0
99cogns <99cogns@naver.com>
2024-08-13 21:51:33 +0900

    feat: boxDetail, boxUpdate 추가

--- src/views/box/BoxCreate.vue
+++ src/views/box/BoxCreate.vue
@@ -1,74 +1,114 @@
 <template>
   <div class="page-container">
-  <v-app class="app-background">
-  <v-container fluid>
-    <div>
-      <h1 class="title">BOX CREATE</h1>
-      </div><br><br>
-    <v-row justify="center">
-      <v-col cols="12" sm="12" md="10">
-        <v-card>
-          <v-card-title class="text-h5 text-center">
-            BOX CREATION
-          </v-card-title>
-          <v-card-text>
-            <v-form @submit.prevent="boxCreate">
-              <v-text-field label="BOX NAME" v-model="boxName" placeholder="박스 이름을 입력해주세요." prepend-icon="mdi-home"
-                required></v-text-field>
+    <v-app class="app-background">
+      <v-container fluid>
+        <div>
+          <h1 class="title">BOX CREATE</h1>
+        </div><br><br>
+        <v-row justify="center">
+          <v-col cols="12" sm="4" md="6">
+            <v-card>
+              <v-card-title class="text-h5 text-center">
+                BOX CREATION
+              </v-card-title>
+              <v-card-text>
+                <v-form @submit.prevent="boxCreate">
+                  <v-text-field 
+                  label="BOX NAME" 
+                  v-model="name" 
+                  placeholder="박스 이름을 입력해주세요." 
+                  prepend-icon="mdi-home"
+                  required>
+                </v-text-field>
 
-              <v-text-field label="BOX ADDRESS" v-model="boxAddress" placeholder="박스의 주소를 입력해주세요."
-                prepend-icon="mdi-map-marker" required></v-text-field>
+                  <v-text-field 
+                  label="BOX ADDRESS" 
+                  v-model="address" 
+                  placeholder="박스의 주소를 입력해주세요."
+                  prepend-icon="mdi-map-marker" 
+                  required>
+                </v-text-field>
 
-              <v-file-input label="BOX LOGO" accept="image/*" @change="fileUpdate" required></v-file-input>
+                  <v-file-input 
+                  label="BOX LOGO" 
+                  accept="image/*" 
+                  @change="fileUpdate" 
+                  required>
+                </v-file-input>
 
-              <v-textarea label="TIME" v-model="useTime" placeholder="박스 이용시간을 입력해주세요." rows="2"
-                prepend-icon="mdi-clock-outline"></v-textarea>
+                  <v-textarea 
+                  label="TIME" 
+                  v-model="operatingHours" 
+                  placeholder="박스 이용시간을 입력해주세요." 
+                  rows="2"
+                  prepend-icon="mdi-clock-outline">
+                </v-textarea>
 
-              <v-textarea label="PRICE" v-model="price" placeholder="₩ 가격을 입력해주세요." rows="2"
-                prepend-icon="mdi-currency-krw"></v-textarea>
+                  <v-textarea 
+                  label="PRICE" 
+                  v-model="fee" 
+                  placeholder="₩ 가격을 입력해주세요." 
+                  rows="2"
+                  prepend-icon="mdi-currency-krw">
+                </v-textarea>
 
-              <v-textarea label="INTRODUCTION" v-model="intro" placeholder="박스를 소개하는 문구를 적어주세요." rows="4"
-                prepend-icon="mdi-pencil-outline"></v-textarea>
+                  <v-textarea 
+                  label="INTRODUCTION" 
+                  v-model="intro" 
+                  placeholder="박스를 소개하는 문구를 적어주세요." 
+                  rows="4"
+                  prepend-icon="mdi-pencil-outline">
+                </v-textarea>
 
-              <v-btn type="submit" color="black" block>CREATE</v-btn>
-            </v-form>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
-</v-app>
-</div>
+                  <v-card-actions>
+                    <v-row justify="center" class="w-100">
+                      <v-col cols="4" class="d-flex justify-center">
+                        <v-btn type="submit" class="mx-auto"
+                          style="background-color: black; color: white; width: 100%;">
+                          CREATE
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-card-actions>
+                </v-form>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app>
+  </div>
 </template>
 
 <script>
-// import axios from 'axios';
+import axios from 'axios';
 export default {
   data() {
     return {
-      boxName: '',
-      boxAddress: '',
-      useTime: '',
-      price: '',
+      name: '',
+      address: '',
+      operatingHours: '',
+      fee: '',
       intro: '',
-      boxLogo: null,
+      logoPath: null,
     };
   },
   methods: {
     fileUpdate(event) {
-      this.boxLogo = event.target.files[0]
+      this.logoPath = event.target.files[0]
     },
     async boxCreate() {
       try {
         let registerData = new FormData();
-        registerData.append("boxName", this.boxName);
-        registerData.append("boxAddress", this.boxAddress);
-        registerData.append("useTime", this.useTime);
-        registerData.append("price", this.price);
+        registerData.append("name", this.name);
+        registerData.append("address", this.address);
+        registerData.append("operatingHours", this.operatingHours);
+        registerData.append("fee", this.fee);
         registerData.append("intro", this.intro);
+        registerData.append("logoPath", this.logoPath);
 
-        // await axios.post(`${process.env.VUE_APP_API_BASE_URL}/box/create`, registerData);
-        // this.$router.push('/box/success');
+        await axios.post(`${process.env.VUE_APP_API_BASE_URL}/box/create`, registerData);
+        this.$router.push('/box/success');
       } catch (e) {
         alert("박스등록에 실패했습니다.");
       }
@@ -80,9 +120,13 @@ export default {
 
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Rubik+Mono+One&display=swap');
+.title {
+    font-family: 'Rubik Mono One', sans-serif;
+  }
 .app-background {
-  background-color: #D9D9D9; 
-  min-height: 100vh; 
+  background-color: #D9D9D9;
+  min-height: 100vh;
   margin: 0;
   padding: 0;
 }
