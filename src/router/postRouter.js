8eee579fb490279843836b65a4eb3e9918b0fef8
Jiho Kim <jiho3634@gmail.com>
2024-08-14 10:00:32 +0900

    router

---
+++ src/router/postRouter.js
@@ -0,0 +1,29 @@
+import PostCreate from "@/views/post/PostCreate.vue";
+import PostDetail from "@/views/post/PostDetail.vue";
+import PostList from "@/views/post/PostList.vue";
+import PostUpdate from "@/views/post/PostUpdate.vue";
+
+export const postRouter = [
+	{
+		path: "/post/create",
+		name: "PostCreate",
+		component: PostCreate,
+	},
+	{
+		path: "/post/list",
+		name: "PostList",
+		component: PostList,
+	},
+	{
+		path: "/post/detail/:id",
+		name: "PostDetail",
+		component: PostDetail,
+		props: true, // route params를 컴포넌트에 props로 전달
+	},
+    {
+        path: '/post/update/:id',
+        name: 'PostUpdate',
+        component: PostUpdate,
+        props: true,
+      }
+];
