a7e731a32555030d34c8e50436cbb648f416b285
qkdrmawll <eunji8106@naver.com>
2024-08-14 12:44:00 +0900

    Feat: 박스 예약 리스트 확장 구현

--- src/views/reservation/ReservationList.vue
+++ src/views/reservation/ReservationList.vue
@@ -1,32 +1,60 @@
 <template>
     <div class="page-container">
-    
+      <div class="box right-align">
+        <br>
+        <span class="boxLocation">
+            CROSSFIT RICH NANGOK
+        </span>
+        <br>
+    </div>
 
     <v-container>
       <div>
-        <h1 class="title">MY</h1>
-        <h1 class="title">RESERVATION</h1>
+        <h1 class="title" style="margin-top: 10px;">RESERVATION</h1>
         </div>
       <v-row>
         <v-col>
           <v-card>
             <v-card-text>
-              <v-table >
+              <v-data-table :headers="tableHeaders" :items="reservationList" class="elevation-1" show-expand hide-default-footer>
+                <template v-slot:[`item.orderStatus`]="{ item }">
+                    <span :class="{ 'text-red': item.orderStatus === 'CANCELD' }">{{ item.orderStatus }}</span>
+                </template>
+              
+                <template v-slot:expanded-row="{item}">
+                    <v-row>
+                        <v-col>
+                          
+                            <v-list-item v-for="detailItem in item.reservationDetails" :key="detailItem.id">
+                                <v-list-item-content>
+                                    <v-list-item-title>
+                                        {{ detailItem.memberName }}
+                                    </v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-col>
+                    </v-row>
+                </template>
+            </v-data-table>
+            <br>
+            <br>
+            <br>
+              <v-table>
                 <thead>
                     <tr>
                       <th style="font-weight: bold;">DATE</th>
                       <th style="font-weight: bold;">TIME</th>
-                      <th style="font-weight: bold;">COACH</th>
+                      <th style="font-weight: bold;">PEOPLE</th>
                       <th style="font-weight: bold;">WOD</th>
-                      <th style="font-weight: bold;">RECORD</th></tr>
+                    </tr>
                 </thead>
                 <tbody>
                     <tr v-for="r in reservationList" :key="r.id">
                     <td>{{r.date}}</td>
                     <td>{{r.time}}</td>
-                    <td>{{r.coachName}}</td>
+                    <td>{{r.maxPeople}}</td>
                     <td><v-btn>view</v-btn></td>
-                    <td v-if="r.recordId">{{r.recordSnF}}</td><td v-else><v-btn>create</v-btn></td></tr>
+                    </tr>
                 </tbody>
             </v-table>
             </v-card-text>
@@ -43,14 +71,20 @@ export default {
     
   data() {
     return {
-      reservationList: []
+      reservationList: [],
+      tableHeaders:[
+                {title:"ID", key:'id', align:'center'},
+                {title:'date',key:'date', align:'center'},
+                {title:'time',key:'time', align:'center'},
+                {title:'maxPeople',key:'maxPeople', align:'center'}
+            ],
     };
   },
   async created() {
-      const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyIiwicm9sZSI6IlVTRVIiLCJpYXQiOjE3MjMxNjA1MDQsImV4cCI6MTcyMzE3ODUwNH0.IwzZ6rz9V7EH58_qaLsykQHqq0FYasqMOdW0fboXzjE';
+
         try {
           console.log(`${process.env.VUE_APP_API_BASE_URL}`);
-            const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/reservation-detail/mylist`,{headers:{Authorization: `Bearer ${token}`}});
+            const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/reservation/box/list/1`);
             console.log(response)
             this.reservationList = response.data.result.content;
         } catch (error) {
@@ -61,18 +95,6 @@ export default {
 </script>
 
 <style>
-.title {
-  font-family: "Roboto", sans-serif;
-  font-size: 100px;
-  line-height: 1.0; /* 전체 텍스트의 줄 간격 설정 */
-}
-
-.page-container {
-    /* 전체 페이지의 배경색을 설정합니다 */
-    background-color: #D9D9D9; /* 원하는 배경색으로 변경 */
-    min-height: 100vh; /* 전체 화면 높이로 설정 */
-  }
-
 .bold {
   font-weight: bold;
 }
