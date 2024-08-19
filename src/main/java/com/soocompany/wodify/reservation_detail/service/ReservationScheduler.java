68e158bd91fbd8c0977b69dc941b5152cd805b40
sueroku <eraltndusn@gmail.com>
2024-08-19 18:07:17 +0900

    pull이 안되넹

--- src/main/java/com/soocompany/wodify/reservation_detail/service/ReservationScheduler.java
+++ src/main/java/com/soocompany/wodify/reservation_detail/service/ReservationScheduler.java
@@ -1,55 +1,55 @@
-package com.soocompany.wodify.reservation_detail.service;
-
-import com.soocompany.wodify.reservation.domain.Reservation;
-import com.soocompany.wodify.reservation.repository.ReservationRepository;
-import com.soocompany.wodify.reservation_detail.controller.SseController;
-import com.soocompany.wodify.reservation_detail.domain.ReservationDetail;
-import com.soocompany.wodify.reservation_detail.dto.ReservationDetailDetResDto;
-import com.soocompany.wodify.reservation_detail.repository.ReservationDetailRepository;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.List;
-
-@Component
-public class ReservationScheduler {
-
-    // 매 분마다..? 한시간 후의 예약을 찾아서
-    // 알림을 날린다.
-    // 스케줄러에 sseController를 넣는다.
-
-//    rList : 예약에서 오늘 날짜의 예약들을 고른다.
-//    rdList : 예약 내역에서 오늘 날짜의 예약들을 통해 예약 내역들을 고른다.
-//    위의 내역들의 시간에 따라 알림을 날린다.
-
-    private final SseController sseController;
-    private final ReservationRepository reservationRepository;
-    private final ReservationDetailRepository reservationDetailRepository;
-
-    public ReservationScheduler(SseController sseController, ReservationRepository reservationRepository, ReservationDetailRepository reservationDetailRepository) {
-        this.sseController = sseController;
-        this.reservationRepository = reservationRepository;
-        this.reservationDetailRepository = reservationDetailRepository;
-    }
-
-    @Scheduled(cron="0 0/1 * * * *")
-    @Transactional
-    public void alarmReservation(){
-        LocalDate date = LocalDate.now();
-        List<Reservation> reservationList = reservationRepository.findAllByDateAndDelYn(date, "N");
-        for(Reservation reservation : reservationList){
-            List<ReservationDetail> reservationDetails = reservationDetailRepository.findByReservationAndDelYn(reservation, "N");
-            for(ReservationDetail reservationDetail : reservationDetails){
-                if(reservationDetail.getReservation().getTime().minusHours(1).equals(LocalTime.now())){
-                    ReservationDetailDetResDto dto = reservationDetail.detFromEntity();
-                    String memberId = String.valueOf(reservationDetail.getMember().getId());
-                    sseController.publishReservationMessage(dto,memberId);
-                }
-            }
-        }
-    }
-
-}
+//package com.soocompany.wodify.reservation_detail.service;
+//
+//import com.soocompany.wodify.reservation.domain.Reservation;
+//import com.soocompany.wodify.reservation.repository.ReservationRepository;
+//import com.soocompany.wodify.reservation_detail.controller.SseController;
+//import com.soocompany.wodify.reservation_detail.domain.ReservationDetail;
+//import com.soocompany.wodify.reservation_detail.dto.ReservationDetailDetResDto;
+//import com.soocompany.wodify.reservation_detail.repository.ReservationDetailRepository;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.time.LocalDate;
+//import java.time.LocalTime;
+//import java.util.List;
+//
+//@Component
+//public class ReservationScheduler {
+//
+//    // 매 분마다..? 한시간 후의 예약을 찾아서
+//    // 알림을 날린다.
+//    // 스케줄러에 sseController를 넣는다.
+//
+////    rList : 예약에서 오늘 날짜의 예약들을 고른다.
+////    rdList : 예약 내역에서 오늘 날짜의 예약들을 통해 예약 내역들을 고른다.
+////    위의 내역들의 시간에 따라 알림을 날린다.
+//
+//    private final SseController sseController;
+//    private final ReservationRepository reservationRepository;
+//    private final ReservationDetailRepository reservationDetailRepository;
+//
+//    public ReservationScheduler(SseController sseController, ReservationRepository reservationRepository, ReservationDetailRepository reservationDetailRepository) {
+//        this.sseController = sseController;
+//        this.reservationRepository = reservationRepository;
+//        this.reservationDetailRepository = reservationDetailRepository;
+//    }
+//
+//    @Scheduled(cron="0 0/1 * * * *")
+//    @Transactional
+//    public void alarmReservation(){
+//        LocalDate date = LocalDate.now();
+//        List<Reservation> reservationList = reservationRepository.findAllByDateAndDelYn(date, "N");
+//        for(Reservation reservation : reservationList){
+//            List<ReservationDetail> reservationDetails = reservationDetailRepository.findByReservationAndDelYn(reservation, "N");
+//            for(ReservationDetail reservationDetail : reservationDetails){
+//                if(reservationDetail.getReservation().getTime().minusHours(1).equals(LocalTime.now())){
+//                    ReservationDetailDetResDto dto = reservationDetail.detFromEntity();
+//                    String memberId = String.valueOf(reservationDetail.getMember().getId());
+//                    sseController.publishReservationMessage(dto,memberId);
+//                }
+//            }
+//        }
+//    }
+//
+//}
