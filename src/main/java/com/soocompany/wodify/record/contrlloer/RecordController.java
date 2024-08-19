5a4e7671b692c825bc73a8b9e662dc5bf4538bbc
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-08-19 17:34:53 +0900

    Update RecordController.java

--- src/main/java/com/soocompany/wodify/record/contrlloer/RecordController.java
+++ src/main/java/com/soocompany/wodify/record/contrlloer/RecordController.java
@@ -22,14 +22,14 @@ public class RecordController {
         this.recordService = recordService;
     }
 
-//    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("hasRole('USER')")
     @PutMapping("/record/create") //있으면 수정 // 없으면 등록 딱 putmapping
     public ResponseEntity<Object> recordCreate(@RequestBody RecordSaveReqDto dto){ // dto안에 등록하고자하는 운동기록의 내용과 예약내역 id가 포함되어 있다. 그걸로 예약내역 불러서 운동기록에 넣어야지
         CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "운동기록이 생성되었습니다.", recordService.recordCreate(dto));
         return new ResponseEntity<>(commonResDto, HttpStatus.OK);
     }
 
-//    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/record/detail/{id}")
     public ResponseEntity<Object> recordDetail(@PathVariable Long id){ // 운동기록의 id
         CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "운동기록 id:"+id+"가 조회되었습니다.", recordService.recordDetail(id));
@@ -44,14 +44,14 @@ public class RecordController {
     }
 
 
-//    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("hasRole('USER')")
     @PatchMapping("/record/update/{id}")
     public ResponseEntity<Object> recordUpdate(@PathVariable Long id, @RequestBody RecordUpdateReqDto dto){  // 운동기록의 id와 운동기록의 수정사항
         CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "운동기록 id:"+id+"가 수정되었습니다.", recordService.recordUpdate(id,dto));
         return new ResponseEntity<>(commonResDto, HttpStatus.OK);
     }
 
-//    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("hasRole('USER')")
     @PatchMapping("/record/delete/{id}")
     public ResponseEntity<Object> recordDelete(@PathVariable Long id){ // 운동기록의 id
         CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "운동기록 id:"+id+"가 삭제되었습니다.", recordService.recordDelete(id));
