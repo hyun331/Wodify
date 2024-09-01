338b4453f4ba4cdb0297d92832e200aa87ff2da7
hyun331 <sus03319@gmail.com>
2024-09-01 16:39:06 +0900

    fix: localhost변경

--- src/router/KakaoLoginUrl.js
+++ src/router/KakaoLoginUrl.js
@@ -1,6 +1,6 @@
 
 const CLIENT_ID="2d129c6af1317e9dc12a8669b1957416";
-const LOGIN_REDIRECT_URL = "http://localhost:8081/member/auth/kakao/callback";
+const LOGIN_REDIRECT_URL = "https://www.wodify.site/member/auth/kakao/callback";
 
 
 export const KAKAO_LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${LOGIN_REDIRECT_URL}`;
