515b8968051719ef4c940371a5d3a2de01428bec
sueroku <eraltndusn@gmail.com>
2024-09-03 21:37:14 +0900

    sse  예약 알림 수정

--- src/main/java/com/soocompany/wodify/reservation_detail/service/ReservationScheduler.java
+++ src/main/java/com/soocompany/wodify/reservation_detail/service/ReservationScheduler.java
@@ -7,10 +7,13 @@ import com.soocompany.wodify.reservation_detail.domain.ReservationDetail;
 import com.soocompany.wodify.reservation_detail.dto.ReservationDetailDetResDto;
 import com.soocompany.wodify.reservation_detail.repository.ReservationDetailRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -24,29 +27,61 @@ public class ReservationScheduler {
     private final ReservationRepository reservationRepository;
     private final ReservationDetailRepository reservationDetailRepository;
 
-    public ReservationScheduler(SseController sseController, ReservationRepository reservationRepository, ReservationDetailRepository reservationDetailRepository) {
+    @Qualifier("schedulerLockTemplate")
+    private final RedisTemplate<String, Object> schedulreRedisTemplate;
+
+    public ReservationScheduler(SseController sseController, ReservationRepository reservationRepository, ReservationDetailRepository reservationDetailRepository,
+                                @Qualifier("schedulerLockTemplate") RedisTemplate<String, Object> schedulreRedisTemplate) {
         this.sseController = sseController;
         this.reservationRepository = reservationRepository;
         this.reservationDetailRepository = reservationDetailRepository;
+        this.schedulreRedisTemplate = schedulreRedisTemplate;
     }
 
     @Scheduled(cron="0 0/1 * * * *") // test
 //    @Scheduled(cron="0 0/5 * * * *")  // test
     @Transactional
     public void alarmReservation(){
-        LocalDate date = LocalDate.now();
-        List<Reservation> reservationList = reservationRepository.findAllByDateAndDelYn(date, "N");
-        for(Reservation reservation : reservationList){
-            List<ReservationDetail> reservationDetails = reservationDetailRepository.findAllByReservationAndDelYn(reservation, "N");
-            for(ReservationDetail reservationDetail : reservationDetails){
-                DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-                if(reservationDetail.getReservation().getTime().minusHours(1).format(dateTimeFormatter).equals(LocalTime.now().format(dateTimeFormatter))){
-                    ReservationDetailDetResDto dto = reservationDetail.detFromEntity();
-                    dto.setCheck("reservationDetail");
-                    String memberId = String.valueOf(reservationDetail.getMember().getId());
-                    sseController.publishReservationMessage(dto,memberId);
+        String lockKey = "reservationLock";
+        Boolean isLocked = schedulreRedisTemplate.opsForValue().setIfAbsent(lockKey, "true", Duration.ofSeconds(60)); // 60초간 락 유지
+        if(Boolean.TRUE.equals(isLocked)){
+            try {
+                System.out.println("서버 1 스케쥴러 시작 ");
+                LocalDate date = LocalDate.now();
+                List<Reservation> reservationList = reservationRepository.findAllByDateAndDelYn(date, "N");
+                for(Reservation reservation : reservationList){
+                    List<ReservationDetail> reservationDetails = reservationDetailRepository.findAllByReservationAndDelYn(reservation, "N");
+                    for(ReservationDetail reservationDetail : reservationDetails){
+                        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+                        if(reservationDetail.getReservation().getTime().minusHours(1).format(dateTimeFormatter).equals(LocalTime.now().format(dateTimeFormatter))){
+                            ReservationDetailDetResDto dto = reservationDetail.detFromEntity();
+                            dto.setCheck("reservationDetail");
+                            String memberId = String.valueOf(reservationDetail.getMember().getId());
+                            sseController.publishReservationMessage(dto,memberId);
+                        }
+                    }
                 }
+                System.out.println("서버 1 스케쥴러 끝 ");
+            }finally {
+                schedulreRedisTemplate.delete(lockKey);
             }
+        }else {
+            System.out.println("다른 인스턴스에서 스케쥴러가 실행 중");
         }
+
+//        LocalDate date = LocalDate.now();
+//        List<Reservation> reservationList = reservationRepository.findAllByDateAndDelYn(date, "N");
+//        for(Reservation reservation : reservationList){
+//            List<ReservationDetail> reservationDetails = reservationDetailRepository.findAllByReservationAndDelYn(reservation, "N");
+//            for(ReservationDetail reservationDetail : reservationDetails){
+//                DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+//                if(reservationDetail.getReservation().getTime().minusHours(1).format(dateTimeFormatter).equals(LocalTime.now().format(dateTimeFormatter))){
+//                    ReservationDetailDetResDto dto = reservationDetail.detFromEntity();
+//                    dto.setCheck("reservationDetail");
+//                    String memberId = String.valueOf(reservationDetail.getMember().getId());
+//                    sseController.publishReservationMessage(dto,memberId);
+//                }
+//            }
+//        }
     }
 }
