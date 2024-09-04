76078debd62aa81610879696e5daeee601fe82cd
Jiho Kim <jiho3634@gmail.com>
2024-09-04 17:04:00 +0900

    fix

--- src/router/Oauth.js
+++ src/router/Oauth.js
@@ -1,4 +1,7 @@
 
 const CLIENT_ID="2d129c6af1317e9dc12a8669b1957416";
-const REDIRECT_URL = "http://localhost:3000/member/auth/kakao/callback";
+
+const REDIRECT_URL = "https://www.wodify.site/member/auth/kakao/callback";
+// const REDIRECT_URL = "http://localhost:3000/member/auth/kakao/callback";
+
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}`;
