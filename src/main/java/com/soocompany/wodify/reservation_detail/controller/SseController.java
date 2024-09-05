bc41b20c944f59815e2880522f425a1506737b4d
sueroku <eraltndusn@gmail.com>
2024-09-05 13:52:17 +0900

    fix

--- src/main/java/com/soocompany/wodify/reservation_detail/controller/SseController.java
+++ src/main/java/com/soocompany/wodify/reservation_detail/controller/SseController.java
@@ -107,33 +107,15 @@ public class SseController implements MessageListener {
 
     //명예의 전당 알림
     public void publishHallOfFameMessage(String memberId) {
-        SseEmitter emitter = emitters.get(memberId);
         ReservationDetailDetResDto dto = ReservationDetailDetResDto.builder()
                 .check("hallOfFame")
                 .build();
-//        if (emitter != null) {
-//            try {
-//                emitter.send(SseEmitter.event().name("hallOfFame").data(dto));
-//            }catch (IOException e){
-//                throw new RuntimeException(e);
-//            }
-//        }else { // 현재 서버의 받는 이의 emitter 정보가 없는 경우
-            sseRedisTemplate.convertAndSend(memberId, dto);
-//        }
+        sseRedisTemplate.convertAndSend(memberId, dto);
     }
 
     //예약 한시간 전 알림
     public void publishReservationMessage(ReservationDetailDetResDto dto, String memberId) {
-        SseEmitter emitter = emitters.get(memberId);
-        if (emitter != null) {
-            try {
-                emitter.send(SseEmitter.event().name("reservationDetail").data(dto));
-            }catch (IOException e){
-                throw new RuntimeException(e);
-            }
-        }else { // 현재 서버의 받는 이의 emitter 정보가 없는 경우
-            sseRedisTemplate.convertAndSend(memberId, dto);
-        }
+        sseRedisTemplate.convertAndSend(memberId, dto);
     }
 
 
